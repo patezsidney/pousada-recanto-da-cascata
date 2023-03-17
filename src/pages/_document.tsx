c702d1d80850ddc713d334543b681e0491148fb6
Sidney Patez <patezsidney.jr@gmail.com>
2023-03-17 15:08:16 -0300

    adicionando google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -40,9 +41,21 @@ export default class MyDocument extends Document {
           href="https://fonts.googleapis.com/css2?family=Abhaya+Libre&family=Allura&family=Julius+Sans+One&family=Montserrat:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
           rel="stylesheet"
         />
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-ND2QP4K');`,
+        }}>
+        </script>
 
       </Head>
 			<body>
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-ND2QP4K"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
 				<Main />
 				<NextScript />
 			</body>
